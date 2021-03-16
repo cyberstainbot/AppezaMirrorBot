1d9094c938e815800c9d4519c04b555e1d6d882e
S M Mahabub Hossain (Priyo) <65087580+SmPriyo@users.noreply.github.com>
2021-03-16 14:14:47 +0600

    python module released as a package to PyPI, added netrc support to d…

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,4 +1,5 @@
-import shutil, psutil
+import shutil
+import psutil
 import signal
 import pickle
 
@@ -6,18 +7,23 @@ from os import execl, path, remove
 from sys import executable
 import time
 
-from telegram.ext import CommandHandler, run_async
-from bot import dispatcher, updater, botStartTime
-from bot.helper.ext_utils import fs_utils
-from bot.helper.telegram_helper.bot_commands import BotCommands
-from bot.helper.telegram_helper.message_utils import *
+from telegram import Update
+from telegram.ext import CallbackContext, CommandHandler, run_async
+from . import dispatcher, updater, botStartTime
+from .helper.ext_utils import fs_utils
+from .helper.telegram_helper.bot_commands import BotCommands
+from .helper.telegram_helper.message_utils import *
 from .helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
 from .helper.telegram_helper.filters import CustomFilters
+from .helper.config import editor
+from .helper.config.subproc import killAll
+from .helper.config import sync
+from .helper.config.dynamic import configList, DYNAMIC_CONFIG
 from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, delete
 
 
 @run_async
-def stats(update, context):
+def stats(update: Update, context: CallbackContext):
     currentTime = get_readable_time((time.time() - botStartTime))
     total, used, free = shutil.disk_usage('.')
     total = get_readable_file_size(total)
@@ -41,26 +47,35 @@ def stats(update, context):
 
 
 @run_async
-def start(update, context):
+def start(update: Update, context: CallbackContext):
     start_string = f'''
-This is a bot which can mirror all your links to Google drive!
+A Bot to mirror files on the internet to Google Drive.
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
     sendMessage(start_string, context.bot, update)
 
 
 @run_async
-def restart(update, context):
-    restart_message = sendMessage("Restarting, Please wait!", context.bot, update)
-    # Save restart message object in order to reply to it after restarting
+def restart(update: Update, context: CallbackContext):
+    restart_msg = sendMessage('Restarting, Please Wait...', context.bot, update)
+    LOGGER.info(f'Restarting the Bot...')
     fs_utils.clean_all()
+    killAll()
+    if DYNAMIC_CONFIG:
+        time.sleep(3)
+        restart_msg.edit_text(f'Syncing to Google Drive...')
+        sync.handler(configList)
+        restart_msg.edit_text(f'Sync Completed!\n{configList}')
+    if not DYNAMIC_CONFIG:
+        time.sleep(5)
+    # Save restart message object in order to reply to it after restarting
     with open('restart.pickle', 'wb') as status:
-        pickle.dump(restart_message, status)
-    execl(executable, executable, "-m", "bot")
+        pickle.dump(restart_msg, status)
+    execl(executable, executable, "-m", "tgmb")
 
 
 @run_async
-def ping(update, context):
+def ping(update: Update, context: CallbackContext):
     start_time = int(round(time.time() * 1000))
     reply = sendMessage("Starting Ping", context.bot, update)
     end_time = int(round(time.time() * 1000))
@@ -68,36 +83,52 @@ def ping(update, context):
 
 
 @run_async
-def log(update, context):
+def log(update: Update, context: CallbackContext):
     sendLogFile(context.bot, update)
 
 
 @run_async
-def bot_help(update, context):
+def bot_help(update: Update, context: CallbackContext):
     help_string = f'''
-/{BotCommands.HelpCommand}: To get this message
+/{BotCommands.StartCommand} Start the bot
 
-/{BotCommands.MirrorCommand} [download_url][magnet_link]: Start mirroring the link to google drive
+/{BotCommands.MirrorCommand} Mirror the provided link to Google Drive
 
-/{BotCommands.UnzipMirrorCommand} [download_url][magnet_link] : starts mirroring and if downloaded file is any archive , extracts it to google drive
+/{BotCommands.UnzipMirrorCommand} Mirror the provided link and if the file is in archive format, it is extracted and then uploaded to Google Drive
 
-/{BotCommands.TarMirrorCommand} [download_url][magnet_link]: start mirroring and upload the archived (.tar) version of the download
+/{BotCommands.TarMirrorCommand} Mirror the provided link and upload in archive format (.tar) to Google Drive
 
-/{BotCommands.WatchCommand} [youtube-dl supported link]: Mirror through youtube-dl. Click /{BotCommands.WatchCommand} for more help.
+/{BotCommands.CancelMirrorCommand} Reply with this command to the source message, and the download will be cancelled
 
-/{BotCommands.TarWatchCommand} [youtube-dl supported link]: Mirror through youtube-dl and tar before uploading
+/{BotCommands.CancelAllCommand} Cancels all running tasks (downloads, uploads, archiving, unarchiving)
 
-/{BotCommands.CancelMirror} : Reply to the message by which the download was initiated and that download will be cancelled
+/{BotCommands.ListCommand} Searches the Google Drive folder for any matches with the search term and presents the search results in a Telegraph page
 
-/{BotCommands.StatusCommand}: Shows a status of all the downloads
+/{BotCommands.StatusCommand} Shows the status of all downloads and uploads in progress
 
-/{BotCommands.ListCommand} [search term]: Searches the search term in the Google drive, if found replies with the link
+/{BotCommands.AuthorizeCommand} Authorize a group chat or, a specific user to use the bot
 
-/{BotCommands.StatsCommand}: Show Stats of the machine the bot is hosted on
+/{BotCommands.UnAuthorizeCommand} Unauthorize a group chat or, a specific user to use the bot
 
-/{BotCommands.AuthorizeCommand}: Authorize a chat or a user to use the bot (Can only be invoked by owner of the bot)
+/{BotCommands.PingCommand} Ping the bot
 
-/{BotCommands.LogCommand}: Get a log file of the bot. Handy for getting crash reports
+/{BotCommands.RestartCommand} Restart the bot
+
+/{BotCommands.StatsCommand} Shows the stats of the machine that the bot is hosted on
+
+/{BotCommands.HelpCommand}: To get the help message
+
+/{BotCommands.LogCommand} Sends the log file of the bot and the log file of 'aria2c' daemon (can be used to analyse crash reports, if any)
+
+/{BotCommands.CloneCommand} Clone folders in Google Drive (owned by someone else) to your Google Drive
+
+/{BotCommands.WatchCommand} Mirror through 'youtube-dl' to Google Drive
+
+/{BotCommands.TarWatchCommand} Mirror through 'youtube-dl' and upload in archive format (.tar) to Google Drive
+
+/{BotCommands.DeleteCommand} Delete files in Google Drive matching the given string
+
+/{BotCommands.ConfigCommand} Edit 'config.env' file
 
 '''
     sendMessage(help_string, context.bot, update)
@@ -108,30 +139,34 @@ def main():
     # Check if the bot is restarting
     if path.exists('restart.pickle'):
         with open('restart.pickle', 'rb') as status:
-            restart_message = pickle.load(status)
-        restart_message.edit_text("Restarted Successfully!")
+            restart_msg = pickle.load(status)
+        restart_msg.edit_text('Sync Completed!\nRestarted Successfully!')
+        LOGGER.info('Restarted Successfully!')
         remove('restart.pickle')
 
     start_handler = CommandHandler(BotCommands.StartCommand, start,
                                    filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
     ping_handler = CommandHandler(BotCommands.PingCommand, ping,
                                   filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
-    restart_handler = CommandHandler(BotCommands.RestartCommand, restart,
-                                     filters=CustomFilters.owner_filter)
+    restart_handler = CommandHandler(BotCommands.RestartCommand, restart, filters=CustomFilters.owner_filter)
     help_handler = CommandHandler(BotCommands.HelpCommand,
                                   bot_help, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
     stats_handler = CommandHandler(BotCommands.StatsCommand,
                                    stats, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
     log_handler = CommandHandler(BotCommands.LogCommand, log, filters=CustomFilters.owner_filter)
+    config_handler = editor.handler
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(ping_handler)
     dispatcher.add_handler(restart_handler)
     dispatcher.add_handler(help_handler)
     dispatcher.add_handler(stats_handler)
     dispatcher.add_handler(log_handler)
+    dispatcher.add_handler(config_handler)
     updater.start_polling()
     LOGGER.info("Bot Started!")
-    signal.signal(signal.SIGINT, fs_utils.exit_clean_up)
+    updater.idle()
+    fs_utils.clean_all()
+    killAll()
 
 
 main()
