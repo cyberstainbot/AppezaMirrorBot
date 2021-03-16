7fcc369c318cf2de86c0c62072d2605a0c0d7302
S M Mahabub Hossain (Priyo) <65087580+SmPriyo@users.noreply.github.com>
2021-03-16 14:25:05 +0600

    python module released as a package to PyPI, added netrc support to d…

--- bot/modules/mirror_status.py
+++ bot/modules/mirror_status.py
@@ -1,15 +1,16 @@
 from telegram.ext import CommandHandler, run_async
-from bot import dispatcher, status_reply_dict, DOWNLOAD_STATUS_UPDATE_INTERVAL, status_reply_dict_lock
-from bot.helper.telegram_helper.message_utils import *
+from tgmb import dispatcher, status_reply_dict, DOWNLOAD_STATUS_UPDATE_INTERVAL, status_reply_dict_lock
+from tgmb.helper.telegram_helper.message_utils import *
 from time import sleep
-from bot.helper.ext_utils.bot_utils import get_readable_message
+from tgmb.helper.ext_utils.bot_utils import get_readable_message
 from telegram.error import BadRequest
-from bot.helper.telegram_helper.filters import CustomFilters
-from bot.helper.telegram_helper.bot_commands import BotCommands
+from tgmb.helper.telegram_helper.filters import CustomFilters
+from tgmb.helper.telegram_helper.bot_commands import BotCommands
 import threading
 
+
 @run_async
-def mirror_status(update,context):
+def mirror_status(update, context):
     message = get_readable_message()
     if len(message) == 0:
         message = "No active downloads"
@@ -21,8 +22,8 @@ def mirror_status(update,context):
         if index in status_reply_dict.keys():
             deleteMessage(bot, status_reply_dict[index])
             del status_reply_dict[index]
-    sendStatusMessage(update,context.bot)
-    deleteMessage(context.bot,update.message)
+    sendStatusMessage(update, context.bot)
+    deleteMessage(context.bot, update.message)
 
 
 mirror_status_handler = CommandHandler(BotCommands.StatusCommand, mirror_status,
