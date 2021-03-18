4f28e96feb9cfe253e69186ada0a8660f3f10894
S M Mahabub Hossain (Priyo) <65087580+SmPriyo@users.noreply.github.com>
2021-03-18 20:45:35 +0000

    Add reddit module
    Initial support

---
+++ bot/modules/reddit.py
@@ -0,0 +1,29 @@
+from pyrogram import filters
+from bot import app, arq
+from bot.utils.errors import capture_err
+from bot.utils.fetch import fetch
+
+__MODULE__ = "Reddit"
+__HELP__ = "/reddit [query] - results something from reddit"
+
+
+@app.on_message(filters.command("reddit") & ~filters.edited)
+@capture_err
+async def reddit(_, message):
+    if len(message.command) != 2:
+        await message.reply_text("/reddit needs an argument")
+        return
+    subreddit = message.text.split(None, 1)[1]
+    try:
+        reddit = await arq.reddit(subreddit)
+        sreddit = reddit.subreddit
+        title = reddit.title
+        image = reddit.url
+        link = reddit.postLink
+        caption = f"""**Title:** `{title}`
+**Subreddit:** {sreddit}
+**PostLink:** {link}"""
+        await message.reply_photo(photo=image, caption=caption)
+    except Exception as e:
+        print(str(e))
+        await message.reply_text(str(e))
