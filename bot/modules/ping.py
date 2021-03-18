82bc1f51620310433fad0be60a10d9be39b3898b
S M Mahabub Hossain (Priyo) <65087580+SmPriyo@users.noreply.github.com>
2021-03-18 20:44:03 +0000

    Add ping module
    Initial support

---
+++ bot/modules/ping.py
@@ -0,0 +1,10 @@
+from pyrogram import filters
+from bot import app
+
+__MODULE__ = "Ping"
+__HELP__ = " /ping - To Check If Bot Is Alive"
+
+
+@app.on_message(filters.command("ping") & ~filters.edited)
+async def ping(_, message):
+    await message.reply_text("Alive!")
