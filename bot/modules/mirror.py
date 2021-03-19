60e9c094368c032b6af3dfab4539a185525a06c7
S M Mahabub Hossain (Priyo) <65087580+SmPriyo@users.noreply.github.com>
2021-03-19 03:58:55 +0000

    Added Customization

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -115,7 +115,7 @@ class MirrorListener(listeners.MirrorListeners):
     def onDownloadError(self, error):
         error = error.replace('<', ' ')
         error = error.replace('>', ' ')
-        LOGGER.info(self.update.effective_chat.id)
+        LOGGER.info(self.update.effective_chat.id)n
         with download_dict_lock:
             try:
                 download = download_dict[self.uid]
@@ -146,13 +146,18 @@ class MirrorListener(listeners.MirrorListeners):
 
     def onUploadComplete(self, link: str, size):
         with download_dict_lock:
-            msg = f'<b>Filename : </b><code>{download_dict[self.uid].name()}</code>\n<b>Size : </b><code>{size}</code>'
+            msg = f'<b>📁 Filename : </b><code>{download_dict[self.uid].name()}</code>\n<b>Size : </b><code>{size}</code>'
+                  f' \n' \
+                  f' 📌 Join To Our Team Drive To Gain Access To The G-Drive Link.\n' \
+                  f' 📌 Do Not Share G-Drive / Index Link Outside The Group. \n' \
+                  f' \n' \
+                  f' 🔰 For Updates Join : @PriyoMirror.\n'
             buttons = button_build.ButtonMaker()
             if SHORTENER is not None and SHORTENER_API is not None:
                 surl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, link)).text
-                buttons.buildbutton("⚡Drive Link⚡", surl)
+                buttons.buildbutton("🔰Drive Link🔰", surl)
             else:
-                buttons.buildbutton("⚡Drive Link⚡", link)
+                buttons.buildbutton("🔰Drive Link🔰", link)
             LOGGER.info(f'Done Uploading {download_dict[self.uid].name()}')
             if INDEX_URL is not None:
                 share_url = requests.utils.requote_uri(f'{INDEX_URL}/{download_dict[self.uid].name()}')
@@ -160,9 +165,9 @@ class MirrorListener(listeners.MirrorListeners):
                     share_url += '/'
                 if SHORTENER is not None and SHORTENER_API is not None:
                     siurl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, share_url)).text
-                    buttons.buildbutton("💥Index Link💥", siurl)
+                    buttons.buildbutton("🔔Index Link🔔", siurl)
                 else:
-                    buttons.buildbutton("💥Index Link💥", share_url)
+                    buttons.buildbutton("🔔Index Link🔔", share_url)
             if BUTTON_THREE_NAME is not None and BUTTON_THREE_URL is not None:
                 buttons.buildbutton(f"{BUTTON_THREE_NAME}", f"{BUTTON_THREE_URL}")
             if BUTTON_FOUR_NAME is not None and BUTTON_FOUR_URL is not None:
