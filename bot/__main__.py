4227be903e64d916de970b328a02a7f442e19265
S M Mahabub Hossain <77372461+iamSMMH@users.noreply.github.com>
2021-12-05 18:18:00 +0600

    Forget

--- bot/__main__.py
+++ bot/__main__.py
@@ -21,7 +21,7 @@ from .helper.ext_utils.telegraph_helper import telegraph
 from .helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
 from .helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper import button_build
-from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, shell, eval, delete, speedtest, count, leech_settings, search
+from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, shell, eval, delete, speedtest, count, leech_settings, search, rssfeeds
 
 
 def stats(update, context):
